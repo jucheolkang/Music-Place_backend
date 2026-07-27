df83e6b6f1663644d24130874e2f7c81a8a2688e
jucheolkang <jucheol9877@naver.com>
2026-07-27 15:41:54 +0900

    fix : 코드 수정

--- src/main/java/org/musicplace/playList/repository/PLRepository.java
+++ src/main/java/org/musicplace/playList/repository/PLRepository.java
@@ -42,62 +42,61 @@ public interface PLRepository extends JpaRepository<PLEntity, Long> {
     /* ================== 목록 조회 ================== */
 
     /** 내 플레이리스트 (삭제 제외) */
-    @Query(
-            value = """
-        SELECT
-            playlist_id   AS playlistId,
-            title         AS PLTitle,
-            nickname,
-            cover_img     AS cover_img,
-            comment,
-            onoff
-        FROM PLAYLIST
-        WHERE member_id = :memberId
-          AND delete_state = false
-        ORDER BY playlist_id DESC
-        """,
-            nativeQuery = true
-    )
-    List<ResponsePLDto> findMyPlaylists(@Param("memberId") String memberId);
+    @Query("""
+        select new org.musicplace.playList.dto.ResponsePLDto(
+            p.playlistId,
+            p.title,
+            p.nickname,
+            p.coverImg,
+            p.onOff,
+            p.comment,
+            p.memberId
+        )
+        from PLEntity p
+        where p.memberId = :memberId
+        and p.deleteState = false
+        order by p.playlistId desc
+    """)
+    List<ResponsePLDto> findMyPlaylists(
+            @Param("memberId") String memberId
+    );
 
     /** 다른 유저 공개 플레이리스트 */
-    @Query(
-            value = """
-        SELECT
-            playlist_id   AS playlistId,
-            title         AS PLTitle,
-            nickname,
-            cover_img     AS cover_img,
-            comment,
-            onoff
-        FROM PLAYLIST
-        WHERE member_id = :memberId
-          AND onoff = 'Public'
-          AND delete_state = false
-        ORDER BY playlist_id DESC
-        """,
-            nativeQuery = true
-    )
+    @Query("""
+        select new org.musicplace.playList.dto.ResponsePLDto(
+            p.playlistId,
+            p.title,
+            p.nickname,
+            p.coverImg,
+            p.onOff,
+            p.comment,
+            p.memberId
+        )
+        from PLEntity p
+        where p.memberId = :memberId
+        and p.onOff = org.musicplace.playList.domain.OnOff.Public
+        and p.deleteState = false
+        order by p.playlistId desc
+    """)
     List<ResponsePLDto> findOtherUserPublicPlaylists(
             @Param("memberId") String memberId
     );
 
     /** 전체 공개 플레이리스트 */
-    @Query(
-            value = """
-        SELECT
-            playlist_id   AS playlistId,
-            title         AS PLTitle,
-            nickname,
-            cover_img     AS cover_img,
-            comment,
-            onoff
-        FROM PLAYLIST
-        WHERE onoff = 'Public'
-          AND delete_state = false
-        ORDER BY playlist_id DESC
-        """,
-            nativeQuery = true
-    )
+    @Query("""
+        select new org.musicplace.playList.dto.ResponsePLDto(
+            p.playlistId,
+            p.title,
+            p.nickname,
+            p.coverImg,
+            p.onOff,
+            p.comment,
+            p.memberId
+        )
+        from PLEntity p
+        where p.onOff = org.musicplace.playList.domain.OnOff.Public
+        and p.deleteState = false
+        order by p.playlistId desc
+    """)
     List<ResponsePLDto> findAllPublicPlaylists();
 }
